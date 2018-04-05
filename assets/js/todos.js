f015dcc19d988a33943eaf0cee95d68daaaa2a5e
Marcos Oliveira <mavinii.cdc@gmail.com>
2018-04-05 17:30:55 -0300

    Function for add function todo List and function remove

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,23 @@
 // esse evento puxa informação do css
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo 
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut("500", function(){		//parent() remove all the parent LI
+		$(this).remove()
+	});		
+	event.stopPropagation();		// add new event with 'stopPropagation()'
+});
+
+// add new element in LI
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		// create a new li and to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>")
+	}
 });
